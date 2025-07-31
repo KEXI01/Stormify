115b8e154085020c6f9e3971b092d163499ec0c1
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-07-31 16:35:45 +0530

    Update __init__.py

--- Opus/platforms/__init__.py
+++ Opus/platforms/__init__.py
@@ -4,10 +4,5 @@ from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
-from .JioSavan import Saavn
 from .Youtube import YouTubeAPI
 
-
-class PlaTForms:
-    def __init__(self):
-        self.saavn = Saavn()
